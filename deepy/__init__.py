b58fe6158ccfa526d1f79a80c9f36b23ef2dd0c3
eitaroyamatsuta <e.yamatsuta49@gmail.com>
2020-04-29 03:23:35 +0900

    first commit

---
+++ deepy/__init__.py
@@ -0,0 +1,13 @@
+is_simple_core = True
+
+if is_simple_core:
+    from deepy.core_simple import Variable
+    from deepy.core_simple import Function
+    from deepy.core_simple import using_config
+    from deepy.core_simple import no_grad
+    from deepy.core_simple import as_array
+    from deepy.core_simple import as_variable
+    from deepy.core_simple import setup_variable
+
+
+setup_variable()
