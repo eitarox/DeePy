4112086e14e46e3117821dcb027fe7597eba2ebb
eitaroyamatsuta <e.yamatsuta49@gmail.com>
2020-06-24 17:49:46 +0900

    Add SGD optimizers

--- deepy/optimizers.py
+++ deepy/optimizers.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Optimizer:
     def __init__(self):
         self.target = None
@@ -12,8 +14,38 @@ class Optimizer:
         for f in self.hooks:
             f(params)
 
-        def update_one(self, param):
-            raise NotImplementedError()
+        for param in params:
+            self.update_one(param)
 
-        def add_hook(self, f):
-            self.hooks.append(f)
+    def update_one(self, param):
+        raise NotImplementedError()
+
+    def add_hook(self, f):
+        self.hooks.append(f)
+
+
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr
+
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
+
+
+class MomentumSGD(Optimizer):
+    def __init__(self, lr=0.01, momentum=0.9):
+        super().__init__()
+        self.lr = lr
+        self.momentum = momentum
+        self.vs = {}
+
+    def update_one(self, param):
+        v_key = id(param)
+        if v_key not in self.vs:
+            self.vs[v_key] = np.zeros_like(param.data)
+
+        v = self.vs[v_key]
+        v *= self.momentum
+        v -= self.lr * param.grad.data
+        param.data += v
